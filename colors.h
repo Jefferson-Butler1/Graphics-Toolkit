912e9540172803f05a3eb9e6e46286860b1dd6cf
Carson Reader <carsonthemonkey@gmail.com>
2024-02-15 10:26:43 -0800

    Adds more colors

--- colors.h
+++ colors.h
@@ -12,10 +12,14 @@
 
 #define BLACK 0, 0, 0
 #define WHITE 1, 1, 1
+#define GRAY 0.5, 0.5, 0.5
+
 #define RED 1, 0, 0
 #define GREEN 0, 1, 0
 #define BLUE 0, 0, 1
 
-
+#define YELLOW 1, 1, 0
+#define MAGENTA 1, 0, 1
+#define CYAN 0, 1, 1
 
 #endif
