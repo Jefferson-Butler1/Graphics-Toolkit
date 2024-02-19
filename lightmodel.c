c5eb5b641afb0f001ff9159c664cebecb8c24589
Carson Reader <carsonthemonkey@gmail.com>
2024-02-19 01:33:06 -0800

    Adds specular highlight to lightmodel

--- lightmodel.c
+++ lightmodel.c
@@ -5,34 +5,25 @@
 #include "vector.h"
 
 const double AMBIENT = 0.2;
-const double SPECULAR_POWER = 0.8;
+const double SPECULAR_POWER = 64;
 
-//TODO: make specular work, and allow the object color to be passed in
+//TODO: allow the object color to be passed in
 Vector3 phong_lighting(Vector3 position, Vector3 normal, Camera cam, PhongLight* lights, int num_lights){
     Vector3 result = {AMBIENT, AMBIENT, AMBIENT};
     for(int l = 0; l < num_lights; l++){
         PhongLight light = lights[l];
         Vector3 light_dir = vec3_normalized(vec3_sub(light.position, position));
-        // return light_dir;
         double dot_prod = vec3_dot_prod(light_dir, normal);
-        if(dot_prod > 0){
-            // printf("yes\n");
-            // Vector3 res = {1, 1, 1};
-            // return res;
-            Vector3 diffuse = vec3_scale(light.diffuse, dot_prod);
-            result = vec3_add(result, diffuse);
-            // vec3_print(position);
-        }
-        // if(dot_prod > 0 || 1){
+        if(dot_prod < 0) continue;
+        Vector3 diffuse = vec3_scale(light.diffuse, dot_prod);
+        result = vec3_add(result, diffuse);
 
-        //     Vector3 reflection = vec3_normalized(vec3_sub(vec3_scale(normal, 2 * dot_prod), light_dir));
-        //     Vector3 view_vec = vec3_normalized(vec3_negated(position));
-        //     double spec = pow(vec3_dot_prod(reflection, view_vec), SPECULAR_POWER);
-        //     if(spec > 0){
-        //         Vector3 specular = vec3_scale(light.specular, spec);
-        //         vec3_add(result, specular);
-        //     }
-        // }
+        Vector3 reflection = vec3_normalized(vec3_sub(vec3_scale(normal, 2 * dot_prod), light_dir));
+        Vector3 view_vec = vec3_normalized(vec3_sub(cam.eye, position));
+        double spec = pow(vec3_dot_prod(reflection, view_vec), SPECULAR_POWER);
+        if(spec < 0) continue;
+        Vector3 specular = vec3_scale(light.specular, spec);
+        result = vec3_add(result, specular);
     }
     return result;
 }
